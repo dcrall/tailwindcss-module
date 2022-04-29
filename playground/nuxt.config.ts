2da3cdce78e03ba42ce260d02c14efab2b6d6e02
Denny Crall <dennis-crall@uiowa.edu>
2022-04-29 16:16:04 -0500

    Upgrade to ais-components@0.2.14

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -6,17 +6,12 @@ export default defineNuxtConfig({
   buildModules: [
     tailwindModule
   ],
-  css: [],
+  css: ['@ais-public/ais-components/styles/uids-fonts.css'],
   tailwindcss: {
     configPath: '@ais-public/ais-components/tailwind.config.js',
-    cssPath: '@ais-public/ais-components/src/css/tailwind.css',
+    cssPath: '@ais-public/ais-components/tailwind.css',
     injectPosition: 0,
-    exposeConfig: false,
-    config: {
-      content: [
-        './node_modules/@ais-public/ais-components/src/**/*.{vue,js,ts,jsx,tsx}'
-      ]
-    }
+    exposeConfig: true
   },
   vite: {
     logLevel: 'info'
